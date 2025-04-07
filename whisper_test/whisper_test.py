874112608b389aa090939465c39fd7a2d3613d23
takuya.iwaki <takuya.iwaki@accenture.com>
2025-04-07 21:12:22 +0900

    タイムスタンプ

--- whisper_test/whisper_test.py
+++ whisper_test/whisper_test.py
@@ -1,10 +1,24 @@
 import whisper
-from jiwer import wer
 
 # model = whisper.load_model("turbo")
-model = whisper.load_model("base")
-result = model.transcribe("5momotarou.mp3")
+model = whisper.load_model("turbo")
+result = model.transcribe("5momotarou.mp3", language="ja", task="transcribe", verbose=False, word_timestamps=True)
 
-with open("large.txt","w") as f:
-    f.write(result["text"])
+# with open("base3.txt","w") as f:
+#     f.write(result["segments"])
 
+with open("turbo.txt","w",encoding="utf-8") as f:
+    # 変換結果の表示
+    for segment in result["segments"]:
+        start = segment['start']
+        end = segment['end']
+        text = segment['text']
+        
+        # 秒を分:秒形式に変換
+        start_min = int(start // 60)
+        start_sec = start % 60
+        end_min = int(end // 60)
+        end_sec = end % 60
+        
+        # 分:秒形式で出力
+        f.writelines(f"[{start_min:02}:{start_sec:06.3f} --> {end_min:02}:{end_sec:06.3f}] {text}\n")
